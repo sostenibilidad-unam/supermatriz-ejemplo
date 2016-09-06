c6b30e3f4f0db2504192a715d7e736a8ca1b0349
serranoycandela <serranoycandela@gmail.com>
2016-09-06 09:25:56 -0700

    making eigenvector and nodes in the constructor

--- supermatrix/__init__.py
+++ supermatrix/__init__.py
@@ -1,9 +1,26 @@
 import numpy as np
+import csv
 
 
 class SuperMatrix:
-    def __init__(self, M=[]):
-        self.M           = np.matrix(M)
+    def __init__(self, path):
+
+        fileReader = csv.reader(open(path), delimiter=",")
+        self.clustersNodos = []
+        renglon = -1
+        cluster = -1
+        self.nodos = []
+        for row in fileReader:
+            renglon = renglon + 1
+            if (renglon > 1):
+                self.nodos.append(row[1])
+                if (row[0] != ""):
+                    cluster = cluster + 1
+                    self.clustersNodos.append({'cluster':row[0], 'nodos':[row[1]]})
+                else:
+                    self.clustersNodos[cluster]['nodos'].append(row[1])
+                    
+        self.M = np.matrix(np.loadtxt(path, delimiter=',', skiprows=2, usecols=range(2, len(self.nodos)+2)))            
         self.eigenvector = self.converge()
 
 
@@ -21,14 +38,12 @@ class SuperMatrix:
         """
         compute eigenvector
         """
-
         p = self.M.copy()
         n = self.M * self.M
-
         while not self.equal(p, n):
             p = n.copy()
             n *= n
-
+            
         return n[:, [0]]
 
 
